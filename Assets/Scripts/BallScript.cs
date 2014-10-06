6bfc319e301ab4d8399e2ebec7873c41d3f51344
Logiant <beaverl@msoe.edu>
2014-10-06 14:52:21 -0500

    Created initial test scene
    Create a block and ball prefab
    Wrote a script to give the ball an initial velocity and knock down a
    small block tower

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = new Vector3 (0, 0, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
