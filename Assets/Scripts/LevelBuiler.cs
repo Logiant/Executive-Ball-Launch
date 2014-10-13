bde690defa655b288c0d2b393d5a8f6627df8dc5
IanMahuta <mahutai@msoe.edu>
2014-10-13 13:50:38 -0500

    Text Reader
    Started script to read level out of text file.

---
+++ Assets/Scripts/LevelBuiler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class LevelBuiler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// next line in the file
+	string line = "";
+
+	// reads in file from user and generates the level
+	private bool load(string filename){
+
+		// create file based on passed in filepath
+		try{
+		File levelFile = new File(filename);
+		}
+		// handle file not found
+		catch (FileNotFoundException fnf){
+			print("File not found");
+		}
+	}
+
+}
