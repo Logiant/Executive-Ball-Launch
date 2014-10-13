c302ed58f4d6e46d181893078e809a0009f106ef
IanMahuta <mahutai@msoe.edu>
2014-10-13 14:21:59 -0500

    Adding gitignore
    Added gitIgnore

--- Assets/Scripts/LevelBuiler.cs
+++ Assets/Scripts/LevelBuiler.cs
@@ -5,19 +5,27 @@ using System.IO;
 
 public class LevelBuiler : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
-	
+		ArrayList levelPaths =  new ArrayList();
 	}
-	// next line in the file
-	string line = "";
+
 
 	// reads in file from user and generates the level
-	private bool load(string filename){
-
+	private bool load(string filepath){
+		//levelPaths.Add (filepath);
+		// next line in the file
+		string line = "";
 		// create file based on passed in filepath
 		try{
-		File levelFile = new File(filename);
+			File levelFile = new File(filepath);
+			StreamReader reader = new StreamReader(levelFile, Encoding.Default);
+			while(line!=null){
+				line = reader.ReadLine ();
+
+			}
 		}
 		// handle file not found
 		catch (FileNotFoundException fnf){
