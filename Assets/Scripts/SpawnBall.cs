b2d7f93a94b16e83fcf208b65882a3a86b658a94
Logan <beaverl@msoe.edu>
2014-10-17 18:57:23 -0500

    Spun potatoes, fixed horrendous bug
    The potato now always fired when the mouse is released. The code I had
    before was an awful placeholder.
    The potato spins with a random angular velocity when launched!

--- Assets/Scripts/SpawnBall.cs
+++ Assets/Scripts/SpawnBall.cs
@@ -10,24 +10,25 @@ public class SpawnBall : MonoBehaviour {
 
 	float speed = 5; //current ball speed
 	int ballsLeft = 5; //number of balls remaining
+	bool charging = false;
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		bool release = false; //true when the ball is launched
 		if (ballsLeft > 0) {
+
 			if (Input.GetMouseButton (0)) { //if the mouse button is down
 				speed = (float)Mathf.Min (maxSpeed, speed + 0.5f); //increment the speed by 0.5 m/s, up to max speed
+				charging = true;
 			}
-			if (Input.GetMouseButtonUp (0)) { //if the mouse button is released
+			else if (charging) { //if the mouse button is released
 				GameObject newBall = (GameObject)Instantiate (ball, transform.position, transform.rotation); //create a new ball
 				newBall.rigidbody.velocity = transform.rotation * new Vector3(0, 0, speed); //set the speed to forward and rotate it
+				newBall.rigidbody.angularVelocity = new Vector3(Random.Range (-8, 8), Random.Range (-8, 8), Random.Range (-8, 8));
 				ballsLeft --; //decrement balls remaining
 				speed = minSpeed; //reset speed
-				release = true; //the mouse was released
+				charging = false;
 			}
-			if (release) { //ensure nothing weird freezes our speed
-				speed = minSpeed; //reset speed
-			}
+
 		}
 		text.text = "Balls Left: " + ballsLeft; //update GUI text every update
 	}
